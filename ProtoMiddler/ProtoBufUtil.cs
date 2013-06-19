23f8e2999c1c2ef99fe3ae84506cc4a5543e33f8
Joe Basirico <joebasirico@gmail.com>
2013-06-19 10:56:10 -0700

    Syncing Jon's latest commits

--- ProtoMiddler/ProtoBufUtil.cs
+++ ProtoMiddler/ProtoBufUtil.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*This file is part of ProtoMiddler
+
+ProtoMiddler is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If not, see http://www.gnu.org/licenses/.
+ 
+Author: Jon Boyd
+Email: jboyd[at]securityinnovation[dot]com
+
+ * */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +36,7 @@ namespace ProtoMiddler
             procStartInfo.RedirectStandardError = true;
             procStartInfo.RedirectStandardOutput = true;
             procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
 
             System.Diagnostics.Process proc = System.Diagnostics.Process.Start(procStartInfo);
 
@@ -61,6 +75,7 @@ namespace ProtoMiddler
             procStartInfo.RedirectStandardError = true;
             procStartInfo.RedirectStandardOutput = true;
             procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
 
             System.Diagnostics.Process proc = System.Diagnostics.Process.Start(procStartInfo);
 
@@ -86,6 +101,7 @@ namespace ProtoMiddler
             procStartInfo.RedirectStandardError = true;
             procStartInfo.RedirectStandardOutput = true;
             procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
 
             System.Diagnostics.Process proc = System.Diagnostics.Process.Start(procStartInfo);
 
